9041fb7b9c04ec50f1b067a55e212fe0b3d0d74f
Ong Jun Xiang <ongjx9@hotmail.com>
2021-08-31 14:16:59 +0800

    convert image post content to string instead of list

--- Scrapy/hardwarezone/hardwarezone/spiders/spider.py
+++ Scrapy/hardwarezone/hardwarezone/spiders/spider.py
@@ -21,7 +21,7 @@ class HWZSpider(scrapy.Spider):
             content = " ".join(text.strip() for text in post.xpath('*//div[@class="bbWrapper"]/text()').getall() if text.strip() != "")
             if content == "":
                 # The post has no text, try finding image posts instead
-                content = post.xpath('*//div[@class="bbWrapper"]/div/img/@src').getall()
+                content = " ".join(post.xpath('*//div[@class="bbWrapper"]/div/img/@src').getall())
 
             yield {
                 'topic': topic, # Retrieving topic passed via meta
