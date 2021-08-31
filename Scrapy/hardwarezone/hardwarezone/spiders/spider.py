ff079d0b58b5cb66e7c1ada288479db69dde73ad
Ong Jun Xiang <ongjx9@hotmail.com>
2021-08-31 14:07:14 +0800

    Update spider to include date of post and post number #
    Update topic retrieval method to fix null values on topic when passing via meta

--- Scrapy/hardwarezone/hardwarezone/spiders/spider.py
+++ Scrapy/hardwarezone/hardwarezone/spiders/spider.py
@@ -8,43 +8,32 @@ class HWZSpider(scrapy.Spider):
     def parse(self, response):
         topic_element_list = response.xpath('//div[@class="structItem-title"]')
         for topic_element in topic_element_list:
-            # encode and decode to remove unicode characters
-            topic = self.remove_unicode(topic_element.xpath("a/text()").get())
+            yield response.follow(topic_element.xpath("a/@href").get(), self.parse)
 
-            yield response.follow(topic_element.xpath("a/@href").get(), self.parse_post, meta={'topic': topic} )
+
+        post_element_list = response.xpath('//div[@class="message-inner"]')
+        topic = response.xpath('//h1[@class="p-title-value"]/text()').get().strip()
+        for post in post_element_list:
+            author = post.xpath('div/section/div/h4/a//text()').get()
+            postNumber = post.xpath('*//ul[has-class("message-attribution-opposite")]/li[2]/a/text()').get().strip()
+            postDate = post.xpath('*//li[@class="u-concealed"]/a/time/@datetime').get()
+
+            content = " ".join(text.strip() for text in post.xpath('*//div[@class="bbWrapper"]/text()').getall() if text.strip() != "")
+            if content == "":
+                # The post has no text, try finding image posts instead
+                content = post.xpath('*//div[@class="bbWrapper"]/div/img/@src').getall()
+
+            yield {
+                'topic': topic, # Retrieving topic passed via meta
+                'author': author,
+                'postNumber': postNumber,
+                'date': postDate,
+                'content': content,
+                'url': response.url
+            }
 
         next_page = response.xpath('//a[has-class("pageNav-jump--next")]/@href').get()
 
         # Retrieve Next Page (Topic Level)
         if next_page is not None:
             yield response.follow(next_page, self.parse)
-
-
-    # callback function to parse post, separate from parsing topics.
-    def parse_post(self, response):
-        post_element_list = response.xpath('//div[@class="message-inner"]')
-
-        for post in post_element_list:
-            author = post.xpath('div/section/div/h4/a//text()').get()
-
-            ## I feel like using css is a better alternative here. It's much shorter.
-            # content = " ".join(self.remove_unicode(text) for text in post.css('article div.bbWrapper::text').getall() if text.strip() != "")
-
-            ## use XPATH - incase we are being marked according to usage of XPATH
-            content = " ".join(self.remove_unicode(text) for text in post.xpath('*//div[@class="bbWrapper"]/text()').getall() if text.strip() != "")
-
-            yield {
-                'author': author,
-                'topic': response.meta.get('topic'), # Retrieving topic passed via meta
-                'content': content
-            }
-
-        next_page = response.xpath('//a[has-class("pageNav-jump--next")]/@href').get()
-
-        # Retrieve Next Page (Post Level)
-        if next_page is not None:
-            yield response.follow(next_page, self.parse_post)
-
-
-    def remove_unicode(self, text):
-        return text.strip().encode("ascii",'ignore').decode()
